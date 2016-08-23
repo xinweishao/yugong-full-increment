f8badff4c231f2f8f127347d59864e285328b9d7
agapple <jianghang.loujh@alibaba-inc.com>
2016-08-23 13:32:18 +0800

    fixed issue #27

--- src/main/java/com/taobao/yugong/common/db/meta/TableMetaGenerator.java
+++ src/main/java/com/taobao/yugong/common/db/meta/TableMetaGenerator.java
@@ -185,7 +185,7 @@ public class TableMetaGenerator {
                     while (rs.next()) {
                         String schema = rs.getString(1);
                         String name = rs.getString(2);
-                        if (!StringUtils.startsWithIgnoreCase(name, "MLOG$_")) {
+                        if (!StringUtils.startsWithIgnoreCase(name, "MLOG$_") && !StringUtils.startsWithIgnoreCase(name, "RUPD$_")) {
                             table = new Table("TABLE", schema, name);
                             result.add(table);
                         }
@@ -202,7 +202,7 @@ public class TableMetaGenerator {
                         String name = rs.getString(3);
                         String type = rs.getString(4);
 
-                        if (!StringUtils.startsWithIgnoreCase(name, "MLOG$_")) {
+                        if (!StringUtils.startsWithIgnoreCase(name, "MLOG$_") && !StringUtils.startsWithIgnoreCase(name, "RUPD$_")) {
                             table = new Table(type, StringUtils.isEmpty(catlog) ? schema : catlog, name);
                             result.add(table);
                         }
