ba8faeb58ab84e0b304e69434af3498ebeb5e78f
caobin <caobin@hujiang.com>
2017-07-17 14:13:07 +0800

    update: data Gen

--- src/test/java/com/taobao/yugong/data/DataGenerator.java
+++ src/test/java/com/taobao/yugong/data/DataGenerator.java
@@ -31,29 +31,30 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.sql.*;
+import java.util.Queue;
 import java.util.Random;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Fill MySQL Data
+ *
  * @author caobin
  * @version 1.0 2017.07.10
  */
 public class DataGenerator {
 
+    private static final int REC_THRESHOLDS = 10;
+
     static {
 
         try {
-            Class<?> driverClass = Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             Throwables.propagate(e);
         }
     }
 
-    private static Connection getConnection(){
+    private static Connection getConnection() {
         try {
             return DriverManager.getConnection("jdbc:mysql://192.168.177.70:3306/test_mysql", "user_acct", "!@#Qaz");
         } catch (SQLException e) {
@@ -62,63 +63,141 @@ public class DataGenerator {
     }
 
 
-    private static void fillShopOrderDetail(int records) throws Exception{
-        while (records -- > 0){
+    private static void fillShopOrderDetail(Integer records) throws Exception {
+        Queue<Integer> queue = null;
+        if (records > REC_THRESHOLDS) {
+            int times = records / REC_THRESHOLDS;
+            queue = new ArrayBlockingQueue<Integer>(times + 1);
+            while (times-- > 0) {
+                queue.add(REC_THRESHOLDS);
+            }
+            int remain = records % REC_THRESHOLDS;
+            if (remain > 0) {
+                queue.add(remain);
+            }
+        } else {
+            queue = new ArrayBlockingQueue<Integer>(1);
+            queue.add(records);
+        }
+
+        while (queue != null) {
+            records = queue.poll();
+            if (records == null) break;
             Connection conn = getConnection();
             PreparedStatement pstmt = null;
-            Random random = new Random();
-
             try {
                 pstmt = conn.prepareStatement("insert into shop_order_detail(order_id, product_id, quantity, unit_cost, is_refunded, product_cate, discount_amount, timestamp) values (?,?,?,?,?,?,?,?)");
+                while (records-- > 0) {
+                    Random random = new Random();
+                    pstmt.setInt(1, Math.abs(random.nextInt(90000000)));
+                    pstmt.setInt(2, Math.abs(random.nextInt(60000000)));
+                    pstmt.setInt(3, Math.abs(random.nextInt(50000)));
+                    pstmt.setBigDecimal(4, new BigDecimal(random.nextFloat(), new MathContext(10, RoundingMode.CEILING)).abs());
+                    pstmt.setBoolean(5, records % 2 == 0);
+                    pstmt.setInt(6, 4);
+                    pstmt.setBigDecimal(7, new BigDecimal(random.nextFloat(), new MathContext(10, RoundingMode.CEILING)).abs());
+                    pstmt.setDate(8, new Date(System.currentTimeMillis()));
 
-                pstmt.setInt(1, Math.abs(random.nextInt(100000)));
-                pstmt.setInt(2, Math.abs(random.nextInt(300000)));
-                pstmt.setInt(3, Math.abs(random.nextInt(1000)));
-                pstmt.setBigDecimal(4, new BigDecimal(random.nextFloat(), new MathContext(10, RoundingMode.CEILING)).abs());
-                pstmt.setBoolean(5, records % 2 == 0);
-                pstmt.setInt(6, 4);
-                pstmt.setBigDecimal(7, new BigDecimal(random.nextFloat(), new MathContext(10, RoundingMode.CEILING)).abs());
-                pstmt.setDate(8, new Date(System.currentTimeMillis()));
-
-                pstmt.executeUpdate();
-            }  finally {
-                if(pstmt != null)pstmt.close();
-                if(conn != null)conn.close();
+                    pstmt.addBatch();
+                }
+                pstmt.executeBatch();
+            } finally {
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
             }
         }
+
     }
 
 
+    private static void fillShopOrderDetailMulti(Integer records) throws Exception {
+        Queue<Integer> queue = null;
+        if (records > REC_THRESHOLDS) {
+            int times = records / REC_THRESHOLDS;
+            queue = new ArrayBlockingQueue<Integer>(times + 1);
+            while (times-- > 0) {
+                queue.add(REC_THRESHOLDS);
+            }
+            int remain = records % REC_THRESHOLDS;
+            if (remain > 0) {
+                queue.add(remain);
+            }
+        } else {
+            queue = new ArrayBlockingQueue<Integer>(1);
+            queue.add(records);
+        }
 
-
-    private static void fillShopOrderDetailMulti(int records) throws Exception{
-        while (records -- > 0){
+        while (queue != null) {
+            records = queue.poll();
+            if (records == null) break;
             Connection conn = getConnection();
             PreparedStatement pstmt = null;
-            Random random = new Random();
-
             try {
                 pstmt = conn.prepareStatement("insert into shop_order_detail_multi(order_id, multi_product_id, product_id, unit_cost) values (?,?,?,?)");
+                while (records-- > 0) {
+                    Random random = new Random();
+                    pstmt.setInt(1, Math.abs(random.nextInt(800000000)));
+                    pstmt.setInt(2, Math.abs(random.nextInt(700000000)));
+                    pstmt.setInt(3, Math.abs(random.nextInt(10000)));
+                    pstmt.setBigDecimal(4, new BigDecimal(random.nextFloat(), new MathContext(10, RoundingMode.CEILING)).abs());
+                    pstmt.addBatch();
 
-                pstmt.setInt(1, Math.abs(random.nextInt(1000000)));
-                pstmt.setInt(2, Math.abs(random.nextInt(2000000)));
-                pstmt.setInt(3, Math.abs(random.nextInt(1000)));
-                pstmt.setBigDecimal(4, new BigDecimal(random.nextFloat(), new MathContext(10, RoundingMode.CEILING)).abs());
+                }
+                pstmt.executeBatch();
 
-                pstmt.executeUpdate();
-            }  finally {
-                if(pstmt != null)pstmt.close();
-                if(conn != null)conn.close();
+            } finally {
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            }
+        }
+    }
+
+    private static void fillNormalDetai(Integer records) throws Exception {
+        Queue<Integer> queue = null;
+        if (records > REC_THRESHOLDS) {
+            int times = records / REC_THRESHOLDS;
+            queue = new ArrayBlockingQueue<Integer>(times + 1);
+            while (times-- > 0) {
+                queue.add(REC_THRESHOLDS);
+            }
+            int remain = records % REC_THRESHOLDS;
+            if (remain > 0) {
+                queue.add(remain);
+            }
+        } else {
+            queue = new ArrayBlockingQueue<Integer>(1);
+            queue.add(records);
+        }
+
+        while (queue != null) {
+            records = queue.poll();
+            if (records == null) break;
+            Connection conn = getConnection();
+            PreparedStatement pstmt = null;
+            try {
+                pstmt = conn.prepareStatement("insert into normal_detail(display_name, amount) values (?,?)");
+                while (records-- > 0) {
+                    Random random = new Random();
+                    pstmt.setString(1, "N" + Math.abs(random.nextInt(800000000)));
+                    pstmt.setString(2, Math.abs(random.nextInt(700000000))+"");
+                    pstmt.addBatch();
+
+                }
+                pstmt.executeBatch();
+
+            } finally {
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
             }
         }
     }
 
 
-    public static void fillData(Tables tables, int recordsPerThread, int threads) throws Exception{
+    public static void fillData(Tables tables, int recordsPerThread, int threads) throws Exception {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(threads);
         ExecutorService executorService = Executors.newFixedThreadPool(threads);
 
-        for(int i = 0; i < threads; i++){
+        for (int i = 0; i < threads; i++) {
             executorService.execute(new DataFiller(tables, cyclicBarrier, recordsPerThread));
         }
 
@@ -126,6 +205,15 @@ public class DataGenerator {
         executorService.awaitTermination(1, TimeUnit.HOURS);
     }
 
+
+
+    enum Tables {
+        ShopOrderDetailMulti,
+
+        ShopOrderDetail,
+
+        NormalDetail
+    }
 
     static class DataFiller implements Runnable {
 
@@ -135,7 +223,7 @@ public class DataGenerator {
 
         private int records;
 
-        public DataFiller(Tables tables, CyclicBarrier cyclicBarrier, int records){
+        public DataFiller(Tables tables, CyclicBarrier cyclicBarrier, int records) {
             this.tables = tables;
             this.cyclicBarrier = cyclicBarrier;
             this.records = records;
@@ -144,32 +232,29 @@ public class DataGenerator {
         @Override
         public void run() {
             try {
-                switch(tables){
+                switch (tables) {
                     case ShopOrderDetail:
                         fillShopOrderDetail(records);
                         break;
                     case ShopOrderDetailMulti:
                         fillShopOrderDetailMulti(records);
                         break;
+                    case NormalDetail:
+                        fillNormalDetai(records);
+                        break;
                     default:
                 }
                 cyclicBarrier.await();
-            } catch (Exception e){
+            } catch (Exception e) {
                 Throwables.propagate(e);
             }
 
         }
     }
 
-    enum Tables {
-        ShopOrderDetailMulti,
-
-        ShopOrderDetail
-    }
-
     public static void main(String[] args) throws Exception {
-       fillData(Tables.ShopOrderDetail, 2000, 20);
-       fillData(Tables.ShopOrderDetailMulti, 2000, 20);
+         //fillData(Tables.ShopOrderDetail, 100000, 10);
+         fillData(Tables.ShopOrderDetailMulti, 10000, 20);
+         fillData(Tables.NormalDetail, 100000, 20);
     }
-
 }
