d5994d497d9ab1cf648f96548d71710fefee5ac7
dijingchao <dijingchao@hujiang.com>
2017-09-07 15:03:35 +0800

    refact: . #ARCH-2058

---
+++ src/main/java/com/taobao/yugong/extractor/mysql/AbstractMysqlExtractor.java
@@ -0,0 +1,7 @@
+package com.taobao.yugong.extractor.mysql;
+
+import com.taobao.yugong.extractor.AbstractFullRecordExtractor;
+
+public class AbstractMysqlExtractor extends AbstractFullRecordExtractor {
+
+}
---
+++ src/main/java/com/taobao/yugong/extractor/sqlserver/AbstractSqlServerExtractor.java
@@ -0,0 +1,7 @@
+package com.taobao.yugong.extractor.sqlserver;
+
+import com.taobao.yugong.extractor.AbstractFullRecordExtractor;
+
+public class AbstractSqlServerExtractor extends AbstractFullRecordExtractor {
+
+}
